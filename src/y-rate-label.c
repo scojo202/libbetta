ebf5fd23fb8bee6baa62732ef3fcf3dd782f127a
Scott O. Johnson <scojo202@gmail.com>
2017-11-06 20:38:23 -0500

    data source for rate label 	modified:   src/y-rate-label.c 	modified:   src/y-rate-label.h

--- src/y-rate-label.c
+++ src/y-rate-label.c
@@ -1,3 +1,24 @@
+/*
+ * y-rate-label.c
+ *
+ * Copyright (C) 2017 Scott O. Johnson (scojo202@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
 #include "y-rate-label.h"
 
 struct _YRateLabel
@@ -9,6 +30,8 @@ struct _YRateLabel
   char i;
   gchar * text;
   gchar * suffix;
+  YData * source;
+  gulong handler;
 };
 
 G_DEFINE_TYPE (YRateLabel, y_rate_label, GTK_TYPE_LABEL)
@@ -17,12 +40,24 @@ static
 void y_rate_label_finalize(GObject *obj)
 {
   YRateLabel *self = Y_RATE_LABEL(obj);
+
+  if(self->source!=NULL) {
+    g_signal_handler_disconnect(self->source,self->handler);
+    g_object_unref(self->source);
+  }
   
   g_timer_destroy(self->timer);
   g_free(self->text);
   
   if (G_OBJECT_CLASS(y_rate_label_parent_class)->finalize)
     G_OBJECT_CLASS(y_rate_label_parent_class)->finalize (obj);
+}
+
+static
+void on_source_changed(YData *data, gpointer user_data)
+{
+  YRateLabel *f = (YRateLabel*) user_data;
+  y_rate_label_update(f);
 }
 
 static void
@@ -49,6 +84,22 @@ YRateLabel * y_rate_label_new(const gchar * text, const gchar *suffix)
   w->text = g_strdup(text);
   w->suffix = g_strdup(suffix);
   return w;
+}
+
+void y_rate_label_set_source(YRateLabel *f, YData *source)
+{
+  if(f->source!=NULL) {
+    g_signal_handler_disconnect(f->source,f->handler);
+    g_object_unref(f->source);
+  }
+  if(source!=NULL) {
+    f->source = g_object_ref_sink(source);
+    f->handler = g_signal_connect(f->source,"changed",G_CALLBACK(on_source_changed),f);
+  }
+  else {
+    f->source = NULL;
+    f->handler = 0;
+  }
 }
 
 void y_rate_label_update(YRateLabel *f)
--- src/y-rate-label.h
+++ src/y-rate-label.h
@@ -1,3 +1,25 @@
+/*
+ * y-rate-label.h
+ *
+ * Copyright (C) 2017 Scott O. Johnson (scojo202@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+#include <y-data.h>
 #include <gtk/gtk.h>
 
 #ifndef __Y_RATE_LABEL_H__
@@ -9,5 +31,6 @@ G_DECLARE_FINAL_TYPE (YRateLabel, y_rate_label, Y, RATE_LABEL, GtkLabel)
 
 YRateLabel * y_rate_label_new(const gchar * text, const gchar *suffix);
 void y_rate_label_update(YRateLabel *f);
+void y_rate_label_set_source(YRateLabel *f, YData *source);
 
 #endif
