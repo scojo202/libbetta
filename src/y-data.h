b59190afa5d4a514f5383d50dd7afa02616e7bdf
Scott O. Johnson <scojo202@gmail.com>
2018-12-03 10:08:36 -0500

    install headers in subdirectories 	modified:   src/data/meson.build 	modified:   src/meson.build 	modified:   src/plot/meson.build 	new file:   src/y-data.h

---
+++ src/y-data.h
@@ -0,0 +1,29 @@
+/*
+ * y-data.h :
+ *
+ * Copyright (C) 2016 Scott O. Johnson (scojo202@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+#ifndef Y_DATA_H
+#define Y_DATA_H
+
+#include <data/y-data-class.h>
+#include <data/y-struct.h>
+#include <data/y-data-simple.h>
+
+#endif
